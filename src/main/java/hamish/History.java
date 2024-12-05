d4d33cebd702b36dd2570d40a0907584076c553a
hamishRodwell <hamishrodwell@hotmail.com>
2024-12-05 04:09:38 +0000

    removed empty history class

--- src/main/java/hamish/History.java
+++
@@ -1,9 +0,0 @@
-package hamish;
-
-import java.util.Stack;
-
-public class History {
-    
-    public Stack<double[]> sheetHistory = new Stack<>();
-    public Stack<String[]> periodsHistory = new Stack<>();
-}
